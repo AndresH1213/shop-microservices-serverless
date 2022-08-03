cb742d06562a4e40807b286ee2f384f5c1222855
Andres Hernandez <edanhebla1213@hotmail.com>
2022-08-02 22:59:36 -0500

    adding basket microservice infrastructure to main stack

--- lib/aws-microservices-stack.ts
+++ lib/aws-microservices-stack.ts
@@ -1,16 +1,28 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+import { SwnDatabase } from './database';
+import { SwnMicroservices } from './microservice';
+import { SwnApiGateway } from './apigateway';
 
 export class AwsMicroservicesStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const database = new SwnDatabase(this, 'Database');
+    const productTable = database.productTable;
+    const basketTable = database.basketTable;
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsMicroservicesQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const microservices = new SwnMicroservices(this, 'Microservice', {
+      productTable,
+      basketTable,
+    });
+    const productFunction = microservices.productFunction;
+    const basketFunction = microservices.basketFunction;
+
+    const apigateway = new SwnApiGateway(this, 'ApiGateway', {
+      productFunction: productFunction,
+      basketFunction: basketFunction,
+    });
   }
 }
