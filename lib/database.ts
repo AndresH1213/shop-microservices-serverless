e402284da1a1de824641f3ca4c2b59fb14945f74
Andres Hernandez <edanhebla1213@hotmail.com>
2022-08-06 16:12:28 -0500

    adding the order table infra

--- lib/database.ts
+++ lib/database.ts
@@ -1,4 +1,4 @@
-import { RemovalPolicy } from 'aws-cdk-lib';
+import { RemovalPolicy, Stack } from 'aws-cdk-lib';
 import {
   AttributeType,
   BillingMode,
@@ -10,20 +10,24 @@ import { Construct } from 'constructs';
 export class SwnDatabase extends Construct {
   public readonly productTable: ITable;
   public readonly basketTable: ITable;
+  public readonly orderTable: ITable;
 
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
     this.productTable = this.createTable('product', 'id');
     this.basketTable = this.createTable('basket', 'username');
+    this.orderTable = this.createTable('order', 'username', 'orderDate');
   }
 
   createTable(name: string, pk: string, sk?: string) {
+    let sortKey = sk ? { name: sk, type: AttributeType.STRING } : undefined;
     return new Table(this, name, {
       partitionKey: {
         name: pk,
         type: AttributeType.STRING,
       },
+      sortKey,
       tableName: name,
       removalPolicy: RemovalPolicy.DESTROY,
       billingMode: BillingMode.PAY_PER_REQUEST,
