881d92c748acf4e4dcbeb924fbfaa7c14029437b
Andres Hernandez <edanhebla1213@hotmail.com>
2022-08-08 21:25:58 -0500

    adding queue and eventbus stacks and ordering microservice

--- lib/aws-microservices-stack.ts
+++ lib/aws-microservices-stack.ts
@@ -4,6 +4,8 @@ import { Construct } from 'constructs';
 import { SwnDatabase } from './database';
 import { SwnMicroservices } from './microservice';
 import { SwnApiGateway } from './apigateway';
+import { SwnEventBus } from './eventbus';
+import { SwnQueue } from './queue';
 
 export class AwsMicroservicesStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -12,17 +14,30 @@ export class AwsMicroservicesStack extends Stack {
     const database = new SwnDatabase(this, 'Database');
     const productTable = database.productTable;
     const basketTable = database.basketTable;
+    const orderTable = database.orderTable;
 
     const microservices = new SwnMicroservices(this, 'Microservice', {
       productTable,
       basketTable,
+      orderTable,
     });
     const productFunction = microservices.productFunction;
     const basketFunction = microservices.basketFunction;
+    const orderingFunction = microservices.orderingFunction;
 
     const apigateway = new SwnApiGateway(this, 'ApiGateway', {
       productFunction: productFunction,
       basketFunction: basketFunction,
+      orderingFunction: orderingFunction,
+    });
+
+    const queue = new SwnQueue(this, 'OrderQueue', {
+      consumer: microservices.orderingFunction,
+    });
+
+    const eventbus = new SwnEventBus(this, 'EventBus', {
+      publisherFunction: microservices.basketFunction,
+      targetQueue: queue.orderQueue,
     });
   }
 }
